4cba69056471360488bb9925e25a14d86dfa4c22
AlifSatriah <azisahyonn@gmail.com>
2024-09-05 15:27:02 +0700

    Create day_004.java

---
+++ day_004.java
@@ -0,0 +1,14 @@
+package day4;
+
+
+public class Day4 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hello");
+        String n ="alif";
+        System.out.printf("nama : %s",n);
+              
+    }
+    
+}
