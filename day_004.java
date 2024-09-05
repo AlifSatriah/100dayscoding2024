174f83c70c3609a22c7b8bb01e82040987d3db72
AlifSatriah <azisahyonn@gmail.com>
2024-09-05 17:12:01 +0800

    Update day_004.java

--- day_004.java
+++ day_004.java
@@ -5,7 +5,7 @@ public class Day4 {
 
     
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.print("hello");
         String n ="alif";
         System.out.printf("nama : %s",n);
               
