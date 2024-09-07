474ab7a70f5f7d49f0ffd57da05453a66357347d
AlifSatriah <azisahyonn@gmail.com>
2024-09-07 17:39:10 +0800

    Create day_006.java

---
+++ day_006.java
@@ -0,0 +1,14 @@
+package day6;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama ="alif satria";
+        int umur =20;
+        double harga =500.00;
+        boolean sayabekerja =true;
+        char inisial ='a';
+        
+    }
+    
+}
