8db0d6868e4d87d4fb44056df2937ae4d7320f73
AlifSatriah <azisahyonn@gmail.com>
2024-09-08 11:13:23 +0800

    Create day_007.java

---
+++ day_007.java
@@ -0,0 +1,14 @@
+package day7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama ="alif satria";
+        int angka =20;
+        
+        System.out.println("nama : " + nama );
+        System.out.println("angka : " + angka);
+        
+    }
+    
+}
