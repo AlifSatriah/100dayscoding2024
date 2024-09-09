f4c9dfbf9ca96e339f267b44ab038f70c7be492a
AlifSatriah <azisahyonn@gmail.com>
2024-09-09 17:59:50 +0700

    Create day_008.java

---
+++ day_008.java
@@ -0,0 +1,23 @@
+package day8;
+
+
+public class Day8 {
+
+    
+    public static void main(String[] args) {
+        // mendeklarasikan dan menginisialisasi variabel
+        String nama = "alif satria";
+        int angka = 20;
+        //mencetak nilai awal
+        System.out.println("nama : "+ nama);
+        System.out.println("angka :"+ angka);
+        
+        //mengupdate nilai variabel
+        nama ="aco satria";
+        angka =30;
+        //mencetak nilai setelah update
+        System.out.println("nama setelah update :" + nama);
+        System.out.println("nama setelah update :" + angka);
+    }
+    
+}
