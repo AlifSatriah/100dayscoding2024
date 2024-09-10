daed20e6efff78323d2a767c9eca2808b89fff34
AlifSatriah <azisahyonn@gmail.com>
2024-09-10 10:25:02 +0700

    Create day_009.java

---
+++ day_009.java
@@ -0,0 +1,13 @@
+package day9;
+
+
+public class Day9 {
+    public static final int MAX_VALUE = 100;
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Max Value: " + MAX_VALUE);
+    }
+    
+}
