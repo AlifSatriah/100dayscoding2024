77d0dcb4617a84dc5be8341c9bf27bec5bf5380b
AlifSatriah <azisahyonn@gmail.com>
2024-09-11 11:41:22 +0700

    Create day_10.java

---
+++ day_10.java
@@ -0,0 +1,33 @@
+package day10;
+
+
+public class Day10 {
+
+    
+    public static void main(String[] args) {
+        byte byte1 = 10;
+        byte byte2 = 20;
+        byte byte3 = 30;
+        byte byte4 = 40;
+        byte byte5 = 50;
+        byte byte6 = 60;
+        byte byte7 = 70;
+        byte byte8 = 80;
+        byte byte9 = 90;
+        byte byte10 = 100;
+        
+        //menampilkan nilai-nilai variabel byte
+        System.out.println("byte1 : " + byte1);
+        System.out.println("byte2 :"  + byte2);
+        System.out.println("byte3 :" + byte3);
+        System.out.println("byte4 :" + byte4);
+        System.out.println("byte5 :" + byte5);
+        System.out.println("byte6 :" + byte6);
+        System.out.println("byte7 :" + byte7);
+        System.out.println("byte8 :" + byte8);
+        System.out.println("byte9 :" + byte9);
+        System.out.println("byte10 :" + byte10);
+        
+    }
+    
+}
