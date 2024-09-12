5529b9298f0ca99de0bdf67ded9355cfb5c4d0f4
AlifSatriah <azisahyonn@gmail.com>
2024-09-12 10:07:57 +0700

    Create day_011.java

---
+++ day_011.java
@@ -0,0 +1,33 @@
+package day11;
+
+
+public class Day11 {
+
+    
+    public static void main(String[] args) {
+        short var1 = 10;
+        short var2 = 20;
+        short var3 = 30;
+        short var4 = 40;
+        short var5 = 50;
+        short var6 = 60;
+        short var7 = 70;
+        short var8 = 80;
+        short var9 = 90;
+        short var10 = 100;
+        short var11 = 110;
+        
+        System.out.println("var1 = " + var1);
+        System.out.println("var2 = " + var2);
+        System.out.println("var3 = " + var3);
+        System.out.println("var4 = " + var4);
+        System.out.println("var5 = " + var5);
+        System.out.println("var6 = " + var6);
+        System.out.println("var7 = " + var7);
+        System.out.println("var8 = " + var8);
+        System.out.println("var9 = " + var9);
+        System.out.println("var10 = " + var10);
+        System.out.println("var11 = " + var11);
+    }
+    
+}
