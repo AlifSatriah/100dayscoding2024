c627fe22e4a3637dabc2923e3689b3eefc23df46
AlifSatriah <azisahyonn@gmail.com>
2024-09-13 12:37:06 +0700

    Create day_012.java

---
+++ day_012.java
@@ -0,0 +1,22 @@
+package day12;
+
+
+public class Day12 {
+
+    
+    public static void main(String[] args) {
+        //baris pertama (Bilangan Ganjil)
+        int apel = 7, jeruk = 5, mangga = 9, pisang = 1, anggur = 3, semangka = 27;
+        System.out.printf("Bilangan Ganjil = %d, %d, %d, %d, %d, %d\n ", apel, jeruk, mangga, pisang, anggur, semangka);
+        
+        //baris kedua (Bilangan Genap)
+        int durian = 8, nanas = 10, rambutan = 4, alpukat = 100;
+        System.out.println("Bilangan Genap = " + durian + "," + nanas + "," + rambutan + "," + alpukat);
+        
+        //baris ketiga (Bilangan Prima)
+        int jambu = 13, belimbing = 23, sirsak = 5, pepaya = 11;
+        System.out.printf("Bilangan Prima = %d, %d, %d, %d\n", jambu, belimbing, sirsak, pepaya);
+        
+    }
+    
+}
