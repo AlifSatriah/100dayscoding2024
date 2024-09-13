3e7b3b0503324550728bde5481b713f588d1892f
AlifSatriah <azisahyonn@gmail.com>
2024-09-13 13:18:11 +0700

    Update day_012.java

--- day_012.java
+++ day_012.java
@@ -6,15 +6,26 @@ public class Day12 {
     
     public static void main(String[] args) {
         //baris pertama (Bilangan Ganjil)
-        int apel = 7, jeruk = 5, mangga = 9, pisang = 1, anggur = 3, semangka = 27;
+        int apel = 7;
+        int jeruk = 5;
+        int mangga = 9;
+        int pisang = 1;
+        int anggur = 3;
+        int semangka = 27;
         System.out.printf("Bilangan Ganjil = %d, %d, %d, %d, %d, %d\n ", apel, jeruk, mangga, pisang, anggur, semangka);
         
         //baris kedua (Bilangan Genap)
-        int durian = 8, nanas = 10, rambutan = 4, alpukat = 100;
+        int durian = 8;
+        int nanas = 10;
+        int rambutan = 4;
+        int alpukat = 100;
         System.out.println("Bilangan Genap = " + durian + "," + nanas + "," + rambutan + "," + alpukat);
         
         //baris ketiga (Bilangan Prima)
-        int jambu = 13, belimbing = 23, sirsak = 5, pepaya = 11;
+        int jambu = 13;
+        int belimbing = 23;
+        int sirsak = 5;
+        int pepaya = 11;
         System.out.printf("Bilangan Prima = %d, %d, %d, %d\n", jambu, belimbing, sirsak, pepaya);
         
     }
