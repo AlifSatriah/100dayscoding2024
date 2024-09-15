297e2d9fed1d8258208c6d6e1fbd97f90c2fc6eb
AlifSatriah <azisahyonn@gmail.com>
2024-09-15 15:06:22 +0700

    Create day_014.java

---
+++ day_014.java
@@ -0,0 +1,41 @@
+package java14;
+
+
+public class Java14 {
+
+    
+    public static void main(String[] args) {
+        //deklarasi 10 variabel bertipe long
+        long var1, var2, var3, var4, var5;
+        long var6, var7, var8, var9, var10;
+        
+        //inisialisasi 10 variabel
+        var1 = 100L;
+        var2 = 200L;
+        var3 = 300L;
+        var4 = 400L;
+        var5 = 500L;
+        var6 = 600L;
+        var7 = 700L;
+        var8 = 800L;
+        var9 = 900L;
+        var10 = 1000L;
+        
+        //mencetak 10 variabel menggunakan printf
+        System.out.printf("var1: %d, var2: %d, var3: %d, var4: %d, var5: %d\n", var1, var2, var3, var4, var5);
+        System.out.printf("var6: %d, var7: %d, var8: %d, var9: %d, var10: %d\n", var6, var7, var8, var9, var10);
+        
+        //inisialisasi dan mencetak 4 variabel sekaligus menggunakan println
+        long var11 = 1100L;
+        long var12 = 1200L;
+        long var13 = 1300L;
+        long var14 = 1400L;
+        
+        System.out.println("var11: " + var11);
+        System.out.println("var12: " + var12);
+        System.out.println("var13: " + var13);
+        System.out.println("var14: " + var14);
+        
+    }
+    
+}
