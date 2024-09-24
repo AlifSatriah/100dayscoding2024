85203d0245bcf7fe97ef1c2cf88f5cc6cea016f1
AlifSatriah <azisahyonn@gmail.com>
2024-09-24 20:22:55 +0700

    Create day_023.java

---
+++ day_023.java
@@ -0,0 +1,56 @@
+package day23;
+
+
+public class Day23 {
+
+    
+    public static void main(String[] args) {
+        //deklarasi variabel tipe data reference long
+        Long var1 = 100l;
+        Long var2 = 200l;
+        Long var3 = 300l;
+        Long var4 = 400l;
+        Long var5 = 500l;
+        
+        //menampilkan nilai awal
+        System.out.println("nilai awal:");
+        System.out.println("var1: " + var1);
+        System.out.println("var2: " + var2);
+        System.out.println("var3: " + var3);
+        System.out.println("var4: " + var4);
+        System.out.println("var5: " + var5);
+        
+        //operasi penambahan pada setiap variabel
+        var1 += 50;
+        var2 += 50;
+        var3 += 50;
+        var4 += 50;
+        var5 += 50;
+        
+        //menampilkan nilai setelah penambahan
+        System.out.println("\nsetelah penambahan 50:");
+        System.out.println("var1: " + var1);
+        System.out.println("var2: " + var2);
+        System.out.println("var3: " + var3);
+        System.out.println("var4: " + var4);
+        System.out.println("var5: " + var5);
+        
+        //operasi pengurangan pada setiap variabel
+        var1 -= 30;
+        var2 -= 30;
+        var3 -= 30;
+        var4 -= 30;
+        var5 -= 30;
+        
+        //menampilkan nilai setelah pengurangan
+        System.out.println("\nsetelah pengurangan 30:");
+        System.out.println("var1: " + var1);
+        System.out.println("var2: " + var2);
+        System.out.println("var3: " + var3);
+        System.out.println("var4: " + var4);
+        System.out.println("var5: " + var5);
+                
+        
+    }
+    
+}
