0240b1e55edb3ebcfef0e5caf666752d427e8cdf
AlifSatriah <azisahyonn@gmail.com>
2024-09-25 19:34:01 +0700

    Create day_24.java

---
+++ day_24.java
@@ -0,0 +1,50 @@
+package day24;
+
+
+public class Day24 {
+
+    
+    public static void main(String[] args) {
+       //deklarasi variabel float
+       Float float1 = 10.5f;
+       Float float2 = 5.3f;
+       Float float3 = 3.2f;
+       Float float4 = 8.7f;
+       Float float5 = 2.4f;
+       
+      // menampilkan nilai awal variabel
+        
+        System.out.println("nilai float1 = " + 1);
+        System.out.println("nilai float2 = " + 2);
+        System.out.println("nilai float3 = " + 3);
+        System.out.println("nilai f;oat4 = " + 4);
+        System.out.println("nilai float5 = " + 5);
+        
+        //operasi penjumlahan
+        Float nilai = float1 + float2 + float3 + float4 + float5;
+        System.out.println("jumlah:" + nilai);
+        
+        //operasi pengurangan
+        Float kurang = float1 - float2;
+        System.out.println("selisih:" + kurang);
+        
+        //operasi perkalian
+        Float perkalian = float2 * float3 ;
+        System.out.println("hasil kali:" + perkalian);
+        
+        //operasi pembagian
+        Float pembagian = float4 / float2;
+        System.out.println("hasil bagi:" + pembagian);
+        
+        //menghitung rata-rata
+        Float rata = nilai/5;
+        System.out.println("rata-rata:" + rata);
+        
+        
+               
+       
+               
+        
+    }
+    
+}
